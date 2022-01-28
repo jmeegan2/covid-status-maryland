40e898a29c39cb6e4a3170a6d086ad9e48aac03b
James Meegan <jmeegan2@owlmail.harford.edu>
2022-01-28 02:48:26 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+init()
+
+function init() {
+
+    var url = "https://corona.lmao.ninja/v2/states/Maryland?yesterday"
+
+    var data = ''
+
+    $.get(url, function (data) {
+        console.log(data)
+
+        data = ` 
+            <td>${data.cases.toLocaleString()}</td>
+            <td>${data.tests.toLocaleString()}</td>
+            <td>${data.deaths.toLocaleString()}</td>
+            <td>${data.population.toLocaleString()}</td>
+            
+            <td id="percentage">${(((data.deaths) / (data.population)) * 100).toLocaleString()}</td>
+            `
+
+        $("#data").html(data)
+
+    })
+}
+
+
+
+function refreshData() {
+    clearData()
+    init()
+}
+
+function clearData() {
+    $("#data").empty()
+    init()
+}
